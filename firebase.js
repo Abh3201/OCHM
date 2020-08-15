d7fcf34ca55be241c5f4f8148eee9a01fa3f8fda
Abh3201 <55826837+Abh3201@users.noreply.github.com>
2020-08-15 20:47:19 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyC25x51SkdHVfLCj4rXqmicUQQy75DC40w",
+    authDomain: "ochm-4f833.firebaseapp.com",
+    databaseURL: "https://ochm-4f833.firebaseio.com",
+    projectId: "ochm-4f833",
+    storageBucket: "ochm-4f833.appspot.com",
+    messagingSenderId: "657768658051",
+    appId: "1:657768658051:web:f1da24057e8aa58fc957b6",
+    measurementId: "G-H932SF9LBH"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
