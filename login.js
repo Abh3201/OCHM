69b601afc302be9e5f7b8a66f9f159e731f39171
Abhishek Yadav <55826837+Abh3201@users.noreply.github.com>
2021-02-12 14:11:25 +0530

    Update login.js

--- login.js
+++ login.js
@@ -3,15 +3,15 @@
 var check = 1;
 
 var firebaseConfig = {
-  apiKey: "AIzaSyC25x51SkdHVfLCj4rXqmicUQQy75DC40w",
-  authDomain: "ochm-4f833.firebaseapp.com",
-  databaseURL: "https://ochm-4f833.firebaseio.com",
-  projectId: "ochm-4f833",
-  storageBucket: "ochm-4f833.appspot.com",
-  messagingSenderId: "657768658051",
-  appId: "1:657768658051:web:f1da24057e8aa58fc957b6",
-  measurementId: "G-H932SF9LBH"
-};
+    apiKey: ,
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
